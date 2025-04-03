78e80b09ce7193fa9102ca4466fa8cf9067ca235
charles <cgiguere@iname.com>
2025-04-03 16:16:59 -0400

    removed unused function

--- JC4827W543_radio_lvgl.ino
+++ JC4827W543_radio_lvgl.ino
@@ -110,18 +110,6 @@ static void lvgl_play_btn_event_cb(lv_event_t *e)
   }
 }
 
-// LVGL calls this function when the play button is pressed
-static void value_changed_event_cb(lv_event_t *e)
-{
-  lv_obj_t *arc = lv_event_get_target_obj(e);
-  lv_obj_t *label = (lv_obj_t *)lv_event_get_user_data(e);
-
-  lv_label_set_text_fmt(label, "%" LV_PRId32 "%%", lv_arc_get_value(arc));
-
-  /*Rotate the label to the current position of the arc*/
-  lv_arc_rotate_obj_to_angle(arc, label, 25);
-}
-
 // Connect to Wi-Fi
 void connectToWiFi()
 {
